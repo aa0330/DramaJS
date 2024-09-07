c679d0255ae9ce93bceed32e7d4ba84c091e38dc
王武侯 <3144216129@qq.com>
2024-09-08 01:13:39 +0800

    链表

--- test.ts
+++ test.ts
@@ -1,8 +1,4 @@
-let map = new Map([
-    [1, 2],
-    [3, 4]
-])
-map.set(6, 7)
-map.set(8, 9)
-console.log(map.keys().next().value);
-
+function a(a: number): number {
+    return a;
+}
+console.log(a(1));
