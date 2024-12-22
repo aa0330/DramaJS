a8a8484e69bafec716a8c97dc049dd090d886220
王武侯 <3144216129@qq.com>
2024-12-22 23:33:25 +0800

    feat:二叉树搜索树 前中后序遍历

--- 原生js/leetCode/07_树系列/02_二叉树.js
+++ 原生js/leetCode/07_树系列/02_二叉树.js
@@ -13,6 +13,7 @@ class createTree {
         for (let val of values) {
             this.inserNode(val)
         }
+        this.list = []
     }
     inserNode(val, curNode = this.root) {
         if (curNode) {
@@ -33,9 +34,34 @@ class createTree {
             this.root = new Node(val)
         }
     }
+    // 前序遍历
+    forFront(curNode = this.root) {
+        if (curNode === null) return;
+        this.list.push(curNode.value)
+        this.forFront(curNode.left)
+        this.forFront(curNode.right)
+    }
+    // 中序遍历
+    formiddle(curNode = this.root) {
+        if (curNode === null) return
+        this.forFront(curNode.left)
+        this.list.push(curNode.value)
+        this.forFront(curNode.right)
+    }
+    // 后续遍历
+    forend(curNode = this.root) {
+        if (curNode === null) return
+        this.forFront(curNode.left)
+        this.forFront(curNode.right)
+        this.list.push(curNode.value)
+    }
+
 }
 
 
 let nums = [9, 5, 10, 15, 4, 3, 9, 11, 14, 15]
-let tree = new createTree(8, 1, 3, 1, 9, 5, 10, 15, 4, 3, 9, 11, 14, 15)
+let tree = new createTree(8, 1, 3, 1, 9, 5, 10)
 console.log(tree);
+
+tree.forend()
+console.log(tree.list);
