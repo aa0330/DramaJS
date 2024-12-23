f274165997edcfb65c09116a586171cb69beac6d
王超凡 <wangchaofan@terminus.io>
2024-12-23 18:01:49 +0800

    todo: 树待完成方法

--- 原生js/leetCode/07_树系列/01_树.js
+++ 原生js/leetCode/07_树系列/01_树.js
@@ -1,46 +1,51 @@
+/**
+ * 增、删、改、查
+ * 树的度、树的高度、节点的度、
+ * 树的层次遍历、 二叉树的前中后序
+ * 翻转二叉树 
+ * 
+ */
+
 //  [1,-1]
 // [1,2,3,4,-1,-1,31,32,41,-1]
 //  [1,2,2,3,4,-1,4]
 
 class Node {
-    constructor(val, location) {
-        this.value = val;
-        this.location = location;
-        this.childs = [];
-    }
+  constructor(val, location) {
+    this.value = val;
+    this.location = location;
+    this.childs = [];
+  }
 }
 
 class Tree {
-    constructor(...values) {
-        if (!values) return;
-        this.root = new Node(values[0], '1-1');
-        this.size = values.length - 1;
-        this.tierArr = [{ tier: 1, sernum: 1 }];
-        let [first, ...resArr] = values;
-        this.initTree(resArr);
+  constructor(...values) {
+    if (!values) return;
+    this.root = new Node(values[0], "1-1");
+    this.size = values.length - 1;
+    this.tierArr = [{ tier: 1, sernum: 1 }];
+    let [first, ...resArr] = values;
+    this.initTree(resArr);
+  }
+
+  initTree(valuesArr) {
+    let curNdoe = this.root;
+    for (let val of valuesArr) {
+      if (val == -1) {
+        // 当前节点不再添加子节点，切换到下一个同层节点或者下一层
+        let [tier, sernum] = curNdoe.location.split("-");
+      } else {
+        let { tier, sernum } = this.tierArr[this.tierArr.length - 1];
+        let newNode = new Node(val, tier + 1 + "-" + sernum);
+        this.tierArr[this.tierArr.length - 1].sernum++;
+        curNdoe.childs.push(newNode);
+      }
     }
+  }
 
-    initTree(valuesArr) {
-        let curNdoe = this.root;
-        for (let val of valuesArr) {
-
-            if (val == -1) {        // 当前节点不再添加子节点，切换到下一个同层节点或者下一层
-                let [tier, sernum] = curNdoe.location.split('-')
-
-
-            } else {
-                let { tier, sernum } = this.tierArr[this.tierArr.length - 1]
-                let newNode = new Node(val, tier + 1 + '-' + sernum);
-                this.tierArr[this.tierArr.length - 1].sernum++;
-                curNdoe.childs.push(newNode);
-            }
-        }
-    }
-
-    addNode(location, value) {
-        let locaArr = location.split('-')
-    }
+  addNode(location, value) {
+    let locaArr = location.split("-");
+  }
 }
-let tree = new Tree(1, 3, 4, 5, -1)
+let tree = new Tree(1, 3, 4, 5, -1);
 console.log(tree);
-
--- 原生js/leetCode/07_树系列/02_二叉树.js
+++ 原生js/leetCode/07_树系列/02_二叉树.js
@@ -1,67 +1,74 @@
 // 搜索二叉树
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = this.right = null;
+  }
 }
 
 class createTree {
-    constructor(...values) {
-        this.root = null;
-        this.size = values.length;
-        for (let val of values) {
-            this.inserNode(val)
-        }
-        this.list = []
+  constructor(...values) {
+    this.root = null;
+    this.size = values.length;
+    for (let val of values) {
+      this.inserNode(val);
     }
-    inserNode(val, curNode = this.root) {
-        if (curNode) {
-            if (curNode.value >= val) {
-                if (curNode.left) {
-                    this.inserNode(val, curNode.left)
-                } else {
-                    curNode.left = new Node(val)
-                }
-            } else {
-                if (curNode.right) {
-                    this.inserNode(val, curNode.right)
-                } else {
-                    curNode.right = new Node(val)
-                }
-            }
+    this.list = [];
+  }
+  inserNode(val, curNode = this.root) {
+    if (curNode) {
+      if (curNode.value >= val) {
+        if (curNode.left) {
+          this.inserNode(val, curNode.left);
         } else {
-            this.root = new Node(val)
+          curNode.left = new Node(val);
         }
+      } else {
+        if (curNode.right) {
+          this.inserNode(val, curNode.right);
+        } else {
+          curNode.right = new Node(val);
+        }
+      }
+    } else {
+      this.root = new Node(val);
     }
-    // 前序遍历
-    forFront(curNode = this.root) {
-        if (curNode === null) return;
-        this.list.push(curNode.value)
-        this.forFront(curNode.left)
-        this.forFront(curNode.right)
+  }
+  // 前序遍历
+  forFront(curNode = this.root) {
+    if (curNode === null) return;
+    this.list.push(curNode.value);
+    this.forFront(curNode.left);
+    this.forFront(curNode.right);
+  }
+  // 中序遍历
+  formiddle(curNode = this.root) {
+    if (curNode === null) return;
+    this.forFront(curNode.left);
+    this.list.push(curNode.value);
+    this.forFront(curNode.right);
+  }
+  // 后续遍历
+  forend(curNode = this.root) {
+    if (curNode === null) return;
+    this.forFront(curNode.left);
+    this.forFront(curNode.right);
+    this.list.push(curNode.value);
+  }
+  // 层次遍历
+  hierTrav() {
+    if (!this.root) return;
+    let quee = [],
+      nodeArr = [this.root];
+    while (curNdoe) {
+      quee.push(curNdoe.val);
     }
-    // 中序遍历
-    formiddle(curNode = this.root) {
-        if (curNode === null) return
-        this.forFront(curNode.left)
-        this.list.push(curNode.value)
-        this.forFront(curNode.right)
-    }
-    // 后续遍历
-    forend(curNode = this.root) {
-        if (curNode === null) return
-        this.forFront(curNode.left)
-        this.forFront(curNode.right)
-        this.list.push(curNode.value)
-    }
-
+  }
 }
 
-
-let nums = [9, 5, 10, 15, 4, 3, 9, 11, 14, 15]
-let tree = new createTree(8, 1, 3, 1, 9, 5, 10)
+let nums = [9, 5, 10, 15, 4, 3, 9, 11, 14, 15];
+let tree = new createTree(8, 1, 3, 1, 9, 5, 10);
 console.log(tree);
 
-tree.forend()
+tree.forend();
 console.log(tree.list);
