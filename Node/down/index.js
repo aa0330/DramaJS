95148dce4d997e6f501117f75c4f041b801bb5f9
王武侯 <3144216129@qq.com>
2025-01-02 23:46:00 +0800

    feat: 常用文件下载

---
+++ Node/down/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import path from 'node:path'
+import fs from 'node:fs'
+
+const app = new express();
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/down', (req, res) => {
+    const fileName = req.body.fileName;
+    const filePath = path.join(process.cwd(), 'static', fileName)
+    const content = fs.readFileSync(filePath)
+    res.setHeader('Content-Type', 'application/octet-stream')
+    res.setHeader('Content-Disposition', 'attachment;filname=' + fileName)
+    res.send(content)
+})
+
+
+app.listen(3000, () => {
+    console.log('3000端口服务已经启动！');
+})
