724099cd1b411e496de9271621d1544e463a86f3
王武侯 <3144216129@qq.com>
2025-10-20 09:08:25 +0800

    1

--- dramaPlayer/index.js
+++ dramaPlayer/index.js
@@ -1,11 +1,27 @@
 class DramaPlayer extends HTMLElement {
-    constructor() {
+    static observedAttributes = ["width"];
+
+    constructor(props) {
         super()
         this.template = document.createElement('template');
         this.template.innerHTML = `<div>66666</div>`;
-        console.log(111, this.template);
+        console.log(111, this.template, this.getAttribute('width'));
         this.appendChild(this.template.content)
     }
+
+    connectedCallback() {
+        console.log('connectedCallback');
+    }
+    disconnectedCallback() {
+        console.log('disconnectedCallback');
+    }
+    adoptedCallback() {
+        console.log('adoptedCallback');
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log('attributeChangedCallback', name, oldValue, newValue);
+    }
+
 
 }
 
